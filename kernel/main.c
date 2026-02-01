160fd3a7d00547faaf0a2774f9088efb7f4a85a4
imbluedabr <baldergroen@gmail.com>
2026-02-01 14:06:05 +0100

    moved init into thread

--- kernel/main.c
+++ kernel/main.c
@@ -2,56 +2,38 @@
 #include <kernel/tty.h>
 #include <kernel/block.h>
 #include <kernel/majors.h>
+#include <kernel/proc.h>
+#include <kernel/syscall.h>
 
-const char msg1[] = "CM2-UNIX V0.0.1\n";
-const char msg2[] = "reg: disk0 at address 0xFFC3\n";
 dev_t tty0_devno;
 struct device* tty0;
-
 dev_t disk0_devno;
 
-struct device_request msg;
+uint8_t init_thread_stack[64];
 
-enum {
-    REG_TTY,
-    REG_DSK,
-    END
-} init_state;
+void init_thread() {
+    char test[] = "CM2-UNIX V0.1.0\nBooting...\n";
+    syscall(0, tty0_devno, (uint32_t) &test, sizeof(test) - 1);
 
-void init_update() {
-    switch(init_state) {
-        case REG_TTY:
-            tty0 = device_create(&tty0_devno, TTY_MAJOR, (void*) 0xFFF1);
-            msg.buffer = msg1;
-            msg.count = sizeof(msg1)-1;
-            msg.operation = DEVICE_OP_WR;
-            device_queue_action(tty0, &msg);
-            init_state = REG_DSK;
-            break;
-        case REG_DSK:
-            if (msg.state != DEVICE_STATE_FINISHED) break;
-            device_create(&disk0_devno, GEN_DISK_MAJOR, (void*) 0xFFC3);
-            msg.buffer = msg2;
-            msg.count = sizeof(msg2)-1;
-            device_queue_action(tty0, &msg);
-            init_state = END;
-            break;
-        case END:
-            break;
-    }
+    while (1) {};
 }
 
-void main(void) {
+void main() {
+    *((uint8_t*) 0xFFF6) = 0;
+    
+    //initialize functions
+    device_init();
     tty_init();
     gen_disk_init();
-    
-    
-    while (1) {
-        init_update();
+    proc_init();
 
-        device_update();
-    }
+    tty0 = device_create(&tty0_devno, TTY_MAJOR, (void*) 0xFFF1);
+    
+    proc_create((uint32_t) &init_thread, (uint32_t) &init_thread_stack[63]);
+    
+    //bootstrap the scheduler by getting the first process to run
+    current_process = proc_dequeue();
+    exit_kernel(); //jump into the current process
 }
-
 
 
