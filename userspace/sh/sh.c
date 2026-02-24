00981c05305143c8fdad11d626649bdbccda3099
imbluedabr <baldergroen@gmail.com>
2026-02-24 15:21:44 +0100

    updated shell

--- userspace/sh/sh.c
+++ userspace/sh/sh.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include <tty.h>
 
 int tty0;
@@ -7,13 +8,18 @@ int tty0;
 
 char line_buffer[LINE_SIZE];
 
-char* argv[4];
+char* argv[5];
 
 int parse_args(char* buffer, int size)
 {
+    memset(argv, 0, sizeof(argv));
+    if (size == 0) { return 0; }
     int argc = 0;
     char* current_arg = buffer;
     for (int i = 0; i < size; i++) {
+        if (argc > 4) {
+            return argc;
+        }
         if (buffer[i] == ' ') {
             buffer[i] = '\0';
             argv[argc++] = current_arg;
@@ -25,17 +31,18 @@ int parse_args(char* buffer, int size)
     return argc;
 }
 
+const char* prompt = "# ";
 
 void main()
 {
     tty0 = open("/dev/tty0", 0);
-    
+    STDOUT = tty0;
+
     while(1) {
         memset(line_buffer, 0, LINE_SIZE);
-        write(tty0, "# ", 2);
+        puts(prompt);
 
         int count = read(tty0, line_buffer, LINE_SIZE);
-
         int argc = parse_args(line_buffer, count);
 
         if (argc == 0) {
@@ -47,8 +54,18 @@ void main()
         } else if (strncmp(argv[0], "clear", LINE_SIZE) == 0) {
             ioctl(tty0, TTY_IOCTL_CLEAR, NULL);
         } else {
-            pid_t new = exec(line_buffer);
-            wait(new);
+            int fileno_vec[] = {
+                tty0,
+                -1
+            };
+
+            //BUG: the return value is not converted to a pid_t(int8_t) properly, this happens somewhere in the kernel im guessing
+            int new = exec(line_buffer, (const char**) argv, fileno_vec);
+            if (new == -1) {
+                printf("sh: %s: command not found\n", argv[0]);
+            } else {
+                wait(new);
+            }
         }
         
     }
